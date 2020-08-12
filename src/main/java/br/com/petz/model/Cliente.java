ccdd14f6407a8fce0a40ca2c72ef4138cdb05b14
Heber Silva <heber.as@gmail.com>
2020-08-12 19:15:52 -0300

    Adicionando anotações SPRING DATA JPA nas Entidades

--- src/main/java/br/com/petz/model/Cliente.java
+++ src/main/java/br/com/petz/model/Cliente.java
@@ -1,10 +1,12 @@
 package br.com.petz.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.Transient;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
@@ -13,18 +15,23 @@ import org.hibernate.validator.constraints.br.CPF;
 
 @Entity
 @Table(name = "cliente")
-public class Cliente {
+public class Cliente { 
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id", nullable = false)
 	private Long id;
 	@NotNull(message = "nome cannot be null")
+	@Column(name = "nome")
 	private String nome;
 	@NotBlank
+	@Column(name = "sexo", nullable = false)
 	private String sexo;
 	@NotEmpty
 	@CPF
+	@Column(name = "cpf", nullable = false)
 	private String cpf;
+	@Column(name = "pet", nullable = false)
 	private String pet;
 
 	public Long getId() {
@@ -33,6 +40,12 @@ public class Cliente {
 
 	public void setId(Long id) {
 		this.id = id;
+	}
+
+	//se retornar "true" é pq não houve persistência
+	@Transient
+	public boolean isNew() { 
+		return null == getId();
 	}
 
 	public String getNome() {
--- src/main/java/br/com/petz/model/Pet.java
+++ src/main/java/br/com/petz/model/Pet.java
@@ -1,57 +1,77 @@
 package br.com.petz.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.Transient;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
 @Entity
 @Table(name = "pet")
-public class Pet {
-
+public class Pet { 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id", nullable = false)
 	private Long id;
 	@NotNull(message = "tipoPet cannot be null")
+	@Column(name = "tipoPet")
 	private String tipoPet;
 	@NotBlank
+	@Column(name = "idade", nullable = false)
 	private String idade;
 	@NotEmpty
+	@Column(name = "produto", nullable = false)
 	private String produto;
+	@Column(name = "valor", nullable = false)
 	private Double valor;
-	
-	
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
+	//se retornar "true" é pq não houve persistência
+	@Transient
+	public boolean isNew() {
+		return null == getId();
+	}
+
 	public String getTipoPet() {
 		return tipoPet;
 	}
+
 	public void setTipoPet(String tipoPet) {
 		this.tipoPet = tipoPet;
 	}
+
 	public String getIdade() {
 		return idade;
 	}
+
 	public void setIdade(String idade) {
 		this.idade = idade;
 	}
+
 	public String getProduto() {
 		return produto;
 	}
+
 	public void setProduto(String produto) {
 		this.produto = produto;
 	}
+
 	public Double getValor() {
 		return valor;
 	}
+
 	public void setValor(Double valor) {
 		this.valor = valor;
 	}
