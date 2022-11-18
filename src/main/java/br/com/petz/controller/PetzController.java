7c474d6f1cd4c70b82d2768321f77dc33d0229fa
Heber Silva <heber.as@gmail.com>
2022-11-18 15:16:32 -0300

    fix

--- src/main/java/br/com/petz/controller/PetzController.java
+++ src/main/java/br/com/petz/controller/PetzController.java
@@ -1,31 +1,19 @@
 package br.com.petz.controller;
 
-import java.util.List;
-
-import javax.management.relation.RoleInfoNotFoundException;
-
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
 import br.com.petz.model.Cliente;
 import br.com.petz.model.Pet;
 import br.com.petz.model.Retorno;
 import br.com.petz.service.PetzService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.management.relation.RoleInfoNotFoundException;
+import java.util.List;
 
 @RestController
 @Api(value = "API PETZ")
